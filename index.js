8ef9af5db364570b05c7a7626e9210e2fc9a6c37
Hung <dokhaihung2003@gmail.com>
2021-10-15 22:59:40 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+var arrIcons = [];
+var posArrI = 0;
+arrIcons.push("dog");
+arrIcons.push("hippo");
+arrIcons.push("paw");
+arrIcons.push("otter");
+arrIcons.push("kiwi-bird");
+arrIcons.push("horse-head");
+arrIcons.push("spider");
+arrIcons.push("horse");
+arrIcons.push("crow");
+arrIcons.push("dragon");
+arrIcons.push("cat");
+var arr = [];
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+//setInterval(()=>{console.log(io.engine.clientsCount)},3000);
+// emit message to all users
+io.on('connection', (socket) => {
+        // Set { <socket.id> }
+        socket.join("room1");
+        console.log(socket.rooms); // Set { <socket.id>, "room1" }
+    socket.on('chat message', (key) => {
+        io.emit('chat message', key);
+    });
+    socket.on('disconnect', () => {
+            var res = arr.find(e => e.id === socket.id);
+            io.emit('out', res,{count:io.engine.clientsCount})
+        })
+        /// push new user after assign id
+    socket.on('push', (key) => {
+            arr.push(key)
+
+        })
+        /// recall join to assign socket.id to new user
+    socket.on('fakejoin', (name) => {
+        io.emit('join', {
+            name: name,
+            icon: arrIcons[posArrI]
+        },{count:io.engine.clientsCount})
+        if (posArrI == arrIcons.length - 1) posArrI = 0;
+        else posArrI++;
+    })
+});
+
+server.listen(5000, () => {
+    console.log('listening on *: http://localhost:5000');
+});
---
+++ public/client.js
@@ -0,0 +1,51 @@
+var socket = io();
+        var messages = document.getElementById('messages');
+        var form = document.getElementById('form');
+        var input = document.getElementById('input');
+        var tagCountUser=document.getElementsByClassName('Room__count--user')[0];
+        var user = {};
+        user.name = prompt("Nhap ten cua ban");
+        if (!user.name) user.name = "người ẩn danh"
+            /// fake participation
+        socket.emit("fakejoin", user.name);
+        // add  submit event
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (input.value) {
+                socket.emit('chat message', {
+                    icon: user.icon,
+                    name: user.name,
+                    message: input.value
+                });
+                input.value = '';
+            }
+        });
+        // log user's message 
+        socket.on('chat message', function(key) {
+            var item = document.createElement('li'); 
+            item.classList.add('container--chat')        
+            item.innerHTML = `<i class="fas fa-${key.icon}  chat--icon"></i> <h3 class="chat__user--name"> ${key.name}</h3> <span class="chat__content"> - ${key.message} </span>`;
+            messages.appendChild(item);
+           
+        });
+        /// log user being out
+        socket.on('out', (key,{count}) => {
+                var item = document.createElement('li');
+                item.innerHTML = `${key.name}  da thoat`;
+                tagCountUser.innerText=count;
+                messages.appendChild(item);
+            })
+            /// notify new user and assign new user's id
+        socket.on('join', (res,{count}) => {
+
+            if (user.id == undefined) /// here  comparing every user to check new user
+            {
+                user.id = socket.id;
+                user.icon = res.icon;
+                socket.emit('push', user)
+            }
+            var item = document.createElement('li');
+            item.innerHTML = `${res.name} da vao room`;
+            tagCountUser.innerText=`${count}`;
+            messages.appendChild(item);
+        })
