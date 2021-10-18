e8ef0e1df92413f1b13436c305e0bf5b39c8906b
Hung <dokhaihung2003@gmail.com>
2021-10-18 11:09:16 +0700

    icon

--- public/client.js
+++ public/client.js
@@ -26,7 +26,7 @@ var socket = io();
         socket.on('chat message', function(key) {
             var item = document.createElement('li'); 
             item.classList.add('container--chat')        
-            item.innerHTML = `<div class="con__chat--icon"> <i class="fas fa-${key.icon}  chat--icon"></i></div> <div class="con__chat--user"><h3 class="chat__user--name"> ${key.name}</div></h3> <span class="chat__content"> - ${key.message} </span>`;
+            item.innerHTML = `<div class="con__chat--icon"> <img src="${key.icon}.png" class=" chat--icon"></div> <div class="con__chat--user"><h3 class="chat__user--name"> ${key.name}</div></h3> <span class="chat__content"> - ${key.message} </span>`;
             messages.appendChild(item);
             window.scrollTo(0,document.body.scrollHeight);
             
